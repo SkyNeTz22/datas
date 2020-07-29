44517f9ee94757db0cd5dd369711baf018cb9bd4
SkyNeTz <skynetz_8@yahoo.ro>
2020-07-29 16:39:42 +0300

    added methods(but fucked up midway)

---
+++ src/main/java/com/shop/barter/crowcoin/controller/AccountController.java
@@ -0,0 +1,47 @@
+package com.shop.barter.crowcoin.controller;
+
+import com.shop.barter.crowcoin.entities.Account;
+import com.shop.barter.crowcoin.repository.AccountInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class AccountController
+    {
+    @Autowired
+    private AccountInterface accountInterface;
+
+    @GetMapping("/getallacc")
+    public List<Account> getAllAccounts()
+    {
+        return new ArrayList<>(accountInterface.findAll());
+    }
+
+    @PostMapping("/addacc")
+    public Account addAccount(@RequestBody Account account)
+    {
+        return accountInterface.save(account);
+    }
+
+    @GetMapping("/findbyid/{id}")
+    public Account findId(@PathVariable Long id)
+    {
+        return accountInterface.findById(id).get();
+    }
+
+    @DeleteMapping("/deleteall/{id}")
+    public Account removeAccount(@PathVariable Long id) {
+        Optional<Account> findbyid = accountInterface.findById(id);
+
+        if (findbyid.isPresent()) {
+            accountInterface.deleteById(id);
+        }
+
+        return null;
+    }
+}
